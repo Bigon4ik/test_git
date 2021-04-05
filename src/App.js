48632f151b9fdb44521f56e358b13f79fb3035b4
Bigon4ik <bigon444ik@gmail.com>
2021-04-05 23:13:07 +0300

    Revert "add h2"
    This reverts commit 8423f9c3cf2b43e8f84bd4c8c5ba0bd9bec07415.
    
    # Conflicts:
    #	src/App.js

--- src/App.js
+++ src/App.js
@@ -2,25 +2,31 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et, porro!</p>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo"/>
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+            </header>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et, porro!</p>
+            <hr/>
+            <p>11</p>
+            <p>42</p>
+            <hr/>
+            <h1>1231231</h1>
+            <h2>2</h2>
+        </div>
+    );
 }
 
 export default App;
